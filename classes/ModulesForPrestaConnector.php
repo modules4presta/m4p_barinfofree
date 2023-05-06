36514eca43c75038d8d32bcefbeeefdd69c48d34
jakub-przepiora <jakub.przepioraa@gmail.com>
2023-05-06 22:35:19 +0200

    change

--- classes/ModulesForPrestaConnector.php
+++ classes/ModulesForPrestaConnector.php
@@ -1,34 +1,34 @@
 <?php
-
-class ModulesForPrestaConnector
-{
-    public static function newInstallation() {
-
-        // @todo implement newInstallation
-
-        return true;
-    }
-
-    public static function checkLicenseForDomain() {
-
-        // @todo implement newInstallation
-        $currentlyDomain = $_SERVER["SERVER_NAME"];
-
-        return $currentlyDomain;
-    }
-
-    public static function sendFeedback($msg) {
-
-        // @todo implement sendFeedback
-
-        return true;
-    }
-
-    public static function getAdsMFP() {
-
-
-        // @todo implement getAdsMFP
-        $ads = [];
-        return $ads;
-    }
-}
+//
+//class ModulesForPrestaConnector
+//{
+//    public static function newInstallation() {
+//
+//        // @todo implement newInstallation
+//
+//        return true;
+//    }
+//
+//    public static function checkLicenseForDomain() {
+//
+//        // @todo implement newInstallation
+//        $currentlyDomain = $_SERVER["SERVER_NAME"];
+//
+//        return $currentlyDomain;
+//    }
+//
+//    public static function sendFeedback($msg) {
+//
+//        // @todo implement sendFeedback
+//
+//        return true;
+//    }
+//
+//    public static function getAdsMFP() {
+//
+//
+//        // @todo implement getAdsMFP
+//        $ads = [];
+//        return $ads;
+//    }
+//}
